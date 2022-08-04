aea70f63e829cfe003430cc81cdfe854ca77c9a3
cybershiptrooper <cybershiptrooper@gmail.com>
2022-08-04 13:59:30 +0530

    minor file fix

---
+++ include/smProgram.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "customerService.h"
+#include "adminService.h"
+#include "smProgramDisplayManager.h"
+
+using namespace std;
+
+class SMProgram{
+	public:
+		void start();
+};
